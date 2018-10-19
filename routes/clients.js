e9f1fbcbb7f3a10d57d1c286e836d87d392ef34c
debrayona <debra.chacaliaz@unmsm.edu.pe>
2018-10-19 01:47:16 -0500

    01.47

--- routes/clients.js
+++ routes/clients.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose =require('mongoose')
 var Client = require('../models/Client.js')
-
+var cliente= Client({_id:"1", client_name:"lucas",client_apellido:"quintana",client_direccion:"ciudad2",client_telefono:"231231455",client_cuentas:{num_cuenta:"74119091"},client_cuentas:{num_cuenta:"1255252342342"}});
+var cliente2= Client({_id:"1", client_name:"cesar",client_apellido:"vallejo",client_direccion:"avenida2",client_telefono:"435635635",client_cuentas:[{num_cuenta:"123456789"},{num_cuenta:"756756756756"}]});
 //Listar
 router.get('/',  async(req,res, next )=>{
 const clients= await Client.find()
@@ -13,18 +14,35 @@ res.json(clients);
   //  res.json(clients)
   })
 //Agregar
-router.post('/', async (req, res)=>{
+router.post('/', async (req, res, next)=>{
   console.log(req.body)
-  const client =new Client(req.body)
+  console.log(cliente)
 
-  await  client.save()
-  res.send('received')
+  const client =new Client(cliente)
+  const client2 = new Client(cliente2)
+  //await cliente.save()
+ await client.save()
+ await client2.save()
+  res.send('post')
   //Client.create(req.body,function(err, post){
-  //  if(err) return next(err);
-  //  res.json(post);
+    //if(err) return next(err);
+   //res.json(post);
 
+   //})
 })
-
+//createError
+router.post('/add' ,(req,res)=>{
+  Client.create(
+    {client_name:req.body.client_name,
+    client_apellido:req.body.client_apellido,
+    client_direccion:req.body.client_direccion,
+    client_telefono:req.body.client_telefono,
+    client_cuentas:req.body.client_cuentas,
+    num_cuenta:req.body.num_cuenta,}
+  ).then(user=>{
+    res.send('add')
+  })
+})
 /* UPDATE PRODUCT */
 router.get('/update/:id', function(req, res, next) {
   Client.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
@@ -50,5 +68,15 @@ router.get('/:id', function(req, res, next) {
     res.json(post);
   });
 });
+
+/* GET SINGLE PRODUCT BY CUENTA */
+router.get('/cuenta/:cuenta', function(req, res, next) {
+  console.log(req.params.cuenta)
+  Client.find({"client_cuentas.num_cuenta":req.params.cuenta}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
 
 module.exports =router;
